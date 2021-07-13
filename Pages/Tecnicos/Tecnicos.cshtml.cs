fc2abb4e81ee8f3e66e6cd59c0ec6f4b59fd7eab
Franz <franzco2000@gmail.com>
2021-07-12 23:39:52 -0400

    feat(Tecnicos): termine

--- Pages/Tecnicos/Tecnicos.cshtml.cs
+++ Pages/Tecnicos/Tecnicos.cshtml.cs
@@ -10,22 +10,23 @@ namespace mantencion.Pages{
     public class MecanicosModel : PageModel {
 
         public List<Mecanico> mecanicos;
-        public List<Mantencion> mantencions;
+        public List<Mantencion> mantenciones;
         public List<MantencionMecanico> mantencionMecanicos;
 
         public void Onget(){
             getData();
         }
 
-        
+
 
         public void getData(){
             BaseDatos baseDatos = new BaseDatos();
 
 
-            mantencions = baseDatos.Mantencions.OrderBy( m => m.id).ToList();
+            mantenciones = baseDatos.Mantencions.OrderBy( m => m.id).ToList();
             mecanicos = baseDatos.Mecanicos.OrderBy( m => m.nombre).ToList();
             mantencionMecanicos = baseDatos.MantencionMecanicos.OrderBy( m => m.id).ToList();
+
         }
     }
 }
