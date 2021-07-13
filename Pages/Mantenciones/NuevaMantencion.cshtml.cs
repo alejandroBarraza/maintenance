16ef780f24766e273c259a8e02c4cac59b673761
Walter Sierra Vega <walter.sierra@alumnos.ucn.cl>
2021-07-12 23:55:00 -0400

    Actualizacion: Ahora se envian y se actualizan correctamente los datos a las tablas correspondientes

--- Pages/Mantenciones/NuevaMantencion.cshtml.cs
+++ Pages/Mantenciones/NuevaMantencion.cshtml.cs
@@ -1,24 +1,25 @@
 using System;
 using System.Collections.Generic;
 using mantencion.Data;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Linq;
 
-namespace mantencion.Pages.Mantenciones{
+namespace mantencion.Pages.Mantenciones
+{
     public class NuevaMantencionModel : PageModel
     {
         public List<Mecanico> Mecanicos; 
+        public List<Material> Materiales;
         
         public void Onget(){
             BaseDatos datos = new BaseDatos();
 
              // Se carga la lista con los mecanicos 
             Mecanicos = datos.Mecanicos.OrderBy( m => m.nombre).ToList();
-            
+            Materiales = datos.Matarials.OrderBy( m => m.nombreMaterial).ToList();
         }
 
-        public void OnPost(DateTime fecha, int id_mecanico,string nombre_material,int cantidad_material,string descripcion){
+        public void OnPost(DateTime fecha, int id_mecanico,int cantidad_material,string descripcion, int horasM, int id_material){
             BaseDatos datos = new BaseDatos();
 
 
@@ -27,27 +28,40 @@ namespace mantencion.Pages.Mantenciones{
             mantencion.fecha = fecha;
             mantencion.descripcion=descripcion;
             datos.Mantencions.Add(mantencion);
-            
-            //ingresar datos a la tabla materiales 
-            Material material = new Material();
-            material.nombreMaterial = nombre_material;
-            material.cantidad = cantidad_material;
-            datos.Matarials.Add(material);
-            datos.SaveChanges();
 
+            // Actualizar cantidad de material
 
+            Material resultado = datos.Matarials.FirstOrDefault(x => x.id == id_material);
+            if(resultado != null){
+                Console.WriteLine("Hola");
+                resultado.cantidad = resultado.cantidad - cantidad_material;
+                Console.WriteLine(resultado.cantidad);
+                datos.SaveChanges();
+            }else{
+                Console.WriteLine(" :( ");
+            }
 
             // // inresar a mantencion material
             MantencionMaterial manMaterial = new MantencionMaterial();
+            manMaterial.id_mantencion = mantencion.id;
+            manMaterial.id_material = id_material;
+            manMaterial.cantidad = cantidad_material;
+            datos.MantencionMaterials.Add(manMaterial);
+
 
 
             // ingresar a mantencion mecanico
             MantencionMecanico manMecanico = new MantencionMecanico();
+            manMecanico.id_mantencion = mantencion.id;
+            manMecanico.id_mecanico = id_mecanico;
+            manMecanico.horas = horasM;
+            datos.MantencionMecanicos.Add(manMecanico);         
             
             
-            
+            datos.SaveChanges();
 
             Mecanicos = datos.Mecanicos.OrderBy( m => m.nombre).ToList();
+            Materiales = datos.Matarials.OrderBy( m => m.nombreMaterial).ToList();
 
         }
 
